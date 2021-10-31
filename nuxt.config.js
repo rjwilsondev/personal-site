2c16746f2e45e6cf72e6667ab3be9c6c9b6e1036
rjwilsondev <ryan@rjwilson.dev>
2021-10-31 16:34:22 -0400

    added nuxt content CMS

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,11 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxt/content'
   ],
+  content: {
+    // Options
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
